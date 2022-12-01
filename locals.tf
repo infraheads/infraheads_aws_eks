7bf36747e8fa6c623759cbc774cf4e430cfe710a
hakob <hakob@pop-os.localdomain>
2022-12-01 18:35:31 +0400

    initial

--- locals.tf
+++ locals.tf
@@ -1,8 +1,11 @@
-locals {
-  vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+# locals {
+#   #-------------------------------------------------------
+#   # VPC
+#   #-------------------------------------------------------
+#   azs      = data.aws_availability_zones.available.names
+#   vpc_id   = var.vpc_id == "" ? module.vpc.vpc_id : var.vpc_id
 
-  tags = {
-    Infraheads  = local.name
-  }
-}
+#   # tags = {
+#   #   Infraheads  = local.name
+#   # }
+# }
