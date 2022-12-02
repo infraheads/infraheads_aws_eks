fb11fdd2ab4a55bcd75caad6e587152bf7103596
Your Name <you@example.com>
2022-12-02 15:50:05 +0400

    Revert "added github_repo and argocd_apps modules"
    This reverts commit a1b144ff14590161c6df4ea05d08b3566e69246d.

--- modules/github-repo/main.tf
+++ modules/github-repo/main.tf
@@ -4,9 +4,13 @@ locals {
 
 # Current version of integrations/github has a bug.
 # The "provider" block needs to be set in the module.
+provider "github" {
+  owner = var.provider_owner
+  token = var.provider_token
+}
 
 resource "github_repository" "repository" {
-  name        = var.github_repo_name
+  name        = var.name
   description = var.description
 
   visibility = var.visibility
