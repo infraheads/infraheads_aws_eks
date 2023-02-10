7e9cc797491102c142cb28fb0b7eb9fe1871a902
Your Name <you@example.com>
2023-02-10 17:16:09 +0400

    comented Vpc

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
-locals {
-  vpc_id                            = var.create_vpc == true ? module.aws_vpc.vpc_id : var.vpc_id
-  private_subnet_ids                = var.create_vpc == true ? module.aws_vpc.*.private_subnet_ids : var.private_subnet_ids
-  public_subnet_ids                 = var.create_vpc == true ? module.aws_vpc.*.public_subnet_ids : var.public_subnet_ids
-  control_plane_subnet_ids          = var.create_vpc == true ? module.aws_vpc.*.public_subnet_ids : var.public_subnet_ids
-  eks_cluster_node_group_subnet_ids = var.create_vpc == true ? module.aws_vpc.*.public_subnet_ids : var.public_subnet_ids
+# locals {
+#   vpc_id                            = var.create_vpc == true ? module.aws_vpc.vpc_id : var.vpc_id
+#   private_subnet_ids                = var.create_vpc == true ? module.aws_vpc.*.private_subnet_ids : var.private_subnet_ids
+#   public_subnet_ids                 = var.create_vpc == true ? module.aws_vpc.*.public_subnet_ids : var.public_subnet_ids
+#   control_plane_subnet_ids          = var.create_vpc == true ? module.aws_vpc.*.public_subnet_ids : var.public_subnet_ids
+#   eks_cluster_node_group_subnet_ids = var.create_vpc == true ? module.aws_vpc.*.public_subnet_ids : var.public_subnet_ids
 
-}
+# }
